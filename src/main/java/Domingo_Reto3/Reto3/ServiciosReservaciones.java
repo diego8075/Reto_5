3e1a0c8ad70765710c651f1765ee14cc940918b4
diego <diego.david.carrillo@gmail.com>
2021-10-28 19:52:05 -0500

    first commit

---
+++ src/main/java/Domingo_Reto3/Reto3/ServiciosReservaciones.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domingo_Reto3.Reto3;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+/**
+ *
+ * @author USUARIO
+ */
+@Service
+public class ServiciosReservaciones {
+       @Autowired
+    private RepositorioReservaciones metodosCrud;
+    
+    public List<Reservaciones> getAll(){
+        return metodosCrud.getAll();
+    }
+
+    public Optional<Reservaciones> getReservation(int reservationId) {
+        return metodosCrud.getReservation(reservationId);
+    }
+
+    public Reservaciones save(Reservaciones reservation){
+        if(reservation.getIdReservation()==null){
+            return metodosCrud.save(reservation);
+        }else{
+            Optional<Reservaciones> e= metodosCrud.getReservation(reservation.getIdReservation());
+            if(e.isEmpty()){
+                return metodosCrud.save(reservation);
+            }else{
+                return reservation;
+            }
+        }
+    }
+    
+    public Reservaciones update(Reservaciones reservaciones){
+        if(reservaciones.getIdReservation()!=null){
+            Optional<Reservaciones> e= metodosCrud.getReservation(reservaciones.getIdReservation());
+            if(!e.isEmpty()){
+
+                if(reservaciones.getStartDate()!=null){
+                    e.get().setStartDate(reservaciones.getStartDate());
+                }
+                if(reservaciones.getDevolutionDate()!=null){
+                    e.get().setDevolutionDate(reservaciones.getDevolutionDate());
+                }
+                if(reservaciones.getStatus()!=null){
+                    e.get().setStatus(reservaciones.getStatus());
+                }
+                metodosCrud.save(e.get());
+                return e.get();
+            }else{
+                return reservaciones;
+            }
+        }else{
+            return reservaciones;
+        }
+    }
+
+    public boolean deleteReservation(int reservationId) {
+        Boolean aBoolean = getReservation(reservationId).map(reservation -> {
+            metodosCrud.delete(reservation);
+            return true;
+        }).orElse(false);
+        return aBoolean;
+    }
+  
+    public StatusReservaciones reporteStatusServicio (){
+        List<Reservaciones>completed= metodosCrud.ReservacionesStatusRepositorio("completed");
+        List<Reservaciones>cancelled= metodosCrud.ReservacionesStatusRepositorio("cancelled");
+        
+        return new StatusReservaciones(completed.size(), cancelled.size() );
+    }
+    
+    public List<Reservaciones> reporteTiempoServicio (String datoA, String datoB){
+        SimpleDateFormat parser = new SimpleDateFormat ("yyyy-MM-dd");
+        
+        Date datoUno = new Date();
+        Date datoDos = new Date();
+        
+        try{
+             datoUno = parser.parse(datoA);
+             datoDos = parser.parse(datoB);
+        }catch(ParseException evt){
+            evt.printStackTrace();
+        }if(datoUno.before(datoDos)){
+            return metodosCrud.ReservacionesTiempoRepositorio(datoUno, datoDos);
+        }else{
+            return new ArrayList<>();
+        
+        } 
+    } 
+     public List<ContadorClientes> reporteClientesServicio(){
+            return metodosCrud.getClientesRepositorio();
+        } 
+}
